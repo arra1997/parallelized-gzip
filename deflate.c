ca226a62963ae11d338c308e4bffaaafb882fc87
jhongucla <Justin@Justins-MacBook-Pro.local>
2018-05-05 13:56:58 -0700

    formatted using GNU coding standards

--- deflate.c
+++ deflate.c
@@ -1,72 +1,103 @@
+/* deflate.c
+
+   Copyright (C) 2018 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
 #include <zlib.h>
-#include "stdlib.h"
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 
-#define windowBits 15
-#define GZIP_ENCODING 16
-int buffer_size = 16384;
+#include "stdlib.h"
 
-void *safe_calloc(size_t nelem, size_t elsize)
+#ifndef WINDOW_BITS
+#  define WINDOW_BITS 15
+#endif
+
+#ifndef GZIP_ENCODING
+#  define GZIP_ENCODING 16
+#endif
+
+#ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 16384
+#endif
+
+void *safe_calloc (size_t nelem, size_t elsize)
 {
-  void *addr = calloc(nelem, elsize);
-  if (addr==NULL)
+  void *addr = calloc (nelem, elsize);
+  if (addr == NULL)
     {
-      printf("Insufficient memory");
-      assert(addr!=NULL);
+      printf ("Insufficient memory");
+      assert (addr != NULL);
     }
   return addr;
 }
 
-static void strm_init (z_stream * strm, int level)
+static void strm_init (z_stream *strm, int level)
 {
     strm->zalloc = Z_NULL;
     strm->zfree  = Z_NULL;
     strm->opaque = Z_NULL;
-    int ret = deflateInit2 (strm, level, Z_DEFLATED,
-                             windowBits | GZIP_ENCODING, 8,
-                             Z_DEFAULT_STRATEGY);
-    if (ret!=Z_OK)
+    int ret = deflateInit2 (strm, level, Z_DEFLATED, 
+                            WINDOW_BITS | GZIP_ENCODING, 8,
+                            Z_DEFAULT_STRATEGY);
+    if (ret != Z_OK)
       exit (EXIT_FAILURE);
 }
 
-int deflate_file(int input_fd, int output_fd, long block_size, int level)
+int deflate_file (int input_fd, int output_fd, long block_size, int level)
 {
   int ret;
   int read_count;
   int write_count;
   z_stream strm;
-  strm_init (& strm, level);
-  unsigned char *in = safe_calloc(buffer_size, sizeof(char));
-  unsigned char *out = safe_calloc(buffer_size, sizeof(char));
-    do {
-        read_count = read(input_fd, in, buffer_size);
-        assert(read_count != -1);
+  strm_init (&strm, level);
+  unsigned char *in = safe_calloc (BUFFER_SIZE, sizeof (char));
+  unsigned char *out = safe_calloc (BUFFER_SIZE, sizeof (char));
+  do 
+    {
+      read_count = read (input_fd, in, BUFFER_SIZE);
+      assert (read_count != -1);
 
-        if (read_count>0) {
-            strm.next_in = in;
-            strm.next_out = out;
-            strm.avail_in = read_count;
-            strm.avail_out = buffer_size;
+      if (read_count > 0) 
+        {
+          strm.next_in = in;
+          strm.next_out = out;
+          strm.avail_in = read_count;
+          strm.avail_out = BUFFER_SIZE;
 
-            if(read_count < buffer_size) {
-                int ret = deflate(&strm, Z_FINISH);
-                assert(ret!=Z_STREAM_ERROR);
-                write_count = write(output_fd, out, buffer_size - strm.avail_out);
-                assert(write_count != -1);
-                break;
+          if (read_count < BUFFER_SIZE) 
+            {
+              int ret = deflate (&strm, Z_FINISH);
+              assert (ret != Z_STREAM_ERROR);
+              write_count = write (output_fd, out, BUFFER_SIZE - strm.avail_out);
+              assert (write_count != -1);
+              break;
             }
 
-            int ret = deflate(&strm, Z_SYNC_FLUSH);
-            assert(ret!=Z_STREAM_ERROR);
-            write_count = write(output_fd, out, buffer_size - strm.avail_out);
-            assert(write_count != -1);
+          int ret = deflate (&strm, Z_SYNC_FLUSH);
+          assert (ret != Z_STREAM_ERROR);
+          write_count = write (output_fd, out, BUFFER_SIZE - strm.avail_out);
+          assert (write_count != -1);
         }
-    } while (read_count > 0);
+    }
+  while (read_count > 0);
 
-  deflateEnd (& strm);
-  free(in);
-  free(out);
+  deflateEnd (&strm);
+  free (in);
+  free (out);
   return 0;
 }
--- gzip.c
+++ gzip.c
@@ -1,11 +1,30 @@
-#include "string.h"
-#include "getopt.h"
-#include "stdlib.h"
-#include "deflate.h"
+/* gzip.c
+
+   Copyright (C) 2018 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+#include "string.h"
+#include "getopt.h"
+#include "stdlib.h"
+#include "deflate.h"
 
 /* How about a struct to map option characters to integer flags?*/
 static const struct option long_options[] =
@@ -16,48 +35,47 @@ static const struct option long_options[] =
     {"blocksize", required_argument, NULL, 'b'},
     {0, 0, 0, 0} //last element has to be all 0s by convention
   };
+
 static char const short_options[] = "ckf";
 int to_stdout = 0;
 int keep = 0;
 int force = 0;
 int level = 6;
-long blocksize = 128;
+long block_size = 128;
 
-int deflate_file();
-
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
   int index = 0;
   int parse_options = 1;
   
   while (parse_options)
     {
-      int opt = getopt_long(argc, argv, short_options, long_options, NULL);
-      switch(opt)
-	{
-	case 'c':
-	  to_stdout = 1;
-	  break;
-	case 'k':
-	  keep = 1;
-	  break;
-	case 'f':
-	  force = 1;
-	  break;
-	case 'b':
-	  blocksize = *optarg;
-	  break;
-	case -1:
-	  parse_options = 0;
-	}
+      int opt = getopt_long (argc, argv, short_options, long_options, NULL);
+      switch (opt)
+      	{
+        	case 'c':
+        	  to_stdout = 1;
+        	  break;
+        	case 'k':
+        	  keep = 1;
+        	  break;
+        	case 'f':
+        	  force = 1;
+        	  break;
+        	case 'b':
+        	  block_size = *optarg;
+        	  break;
+        	case -1:
+        	  parse_options = 0;
+      	}
     }
   for (index = optind; index < argc; index++)
     {
-      int input_flag = O_RDONLY | (force?O_NOFOLLOW:0);
+      int input_flag = O_RDONLY | (force ? O_NOFOLLOW : 0);
       int output_flag = O_WRONLY | O_CREAT;
-      int i_fd = open(argv[index], input_flag);
-      int o_fd = open(strcat(argv[index], ".gz"), output_flag);
-      deflate_file(i_fd, o_fd, blocksize*128, level);
+      int i_fd = open (argv[index], input_flag);
+      int o_fd = open (strcat (argv[index], ".gz"), output_flag);
+      deflate_file (i_fd, o_fd, block_size * 128, level);
     }
 }
 
