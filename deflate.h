b3a0d3e46c1783a418526dfe712c5240bb4a8ea1
jhongucla <justinjh@ucla.edu>
2018-05-07 20:18:36 -0700

    incorporated zlib into gzip

--- deflate.h
+++ deflate.h
@@ -17,4 +17,3 @@
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
 int deflate_file (int input_fd, int output_fd, long block_size, int level);
-int inflate_file (int input_fd, int output_fd);
